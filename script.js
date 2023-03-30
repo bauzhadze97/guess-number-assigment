2ae9e84077c187d271cf20c9b5d281c190d8847b
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-30 21:48:59 +0400

    Done Gues Number

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const inputNumber = document.querySelector('.guess').value;
+const inputNumber = document.querySelector('.guess');
 const Checkbutton = document.querySelector('.check');
 const randomNumber = Math.floor(Math.random() * 100) + 1;
-const score = document.querySelector('.score');
-console.log(randomNumber);
+const AgainButton = document.querySelector('.again');
 
-
-Checkbutton.addEventListener('click', () => {
-    if (inputNumber === randomNumber) {
-        score.textContent = 'You Win!';
-    } else if (inputNumber > randomNumber) {
-        score.textContent = 'You Lose!';
-    } else if (inputNumber < randomNumber) {
-        score.textContent = 'You lose!';
+Checkbutton.addEventListener('click', function () {
+    const GuessNumber = Number(inputNumber.value);
+    let score = 0;
+    let highscore = 0;
+   if (!GuessNumber) {
+        document.querySelector('.message').textContent = "⛔ No number!";
     }
+  else if (GuessNumber === randomNumber) {
+    score ++;
+    highscore ++;
+    document.body.style.backgroundColor = '#60b347';    
+    document.querySelector('.number').textContent = randomNumber;
+    document.querySelector('.message').textContent = "🎉 Correct number!";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.highscore').textContent = highscore;
+  } else if (GuessNumber > randomNumber) {
+    document.querySelector('.message').textContent = "📈 Too high!";
+  } else if (GuessNumber < randomNumber) {
+    document.querySelector('.message').textContent = "📉 Too low!";
+  }
 });
 
+AgainButton.addEventListener('click', function () {
+    document.body.style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('.highscore').textContent = 0;
+    inputNumber.value = '';
+});
