5c8828fe5fdf98fe86cb3e15dc0fabaa407eac67
bauzhadze97 <bauzhadzedev@gmail.com>
2023-03-30 23:23:30 +0400

    Working on again

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const inputNumber = document.querySelector('.guess');
 const Checkbutton = document.querySelector('.check');
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 const AgainButton = document.querySelector('.again');
+let score = 0;
+let highscore = 0;
 
 Checkbutton.addEventListener('click', function () {
     const GuessNumber = Number(inputNumber.value);
-    let score = 0;
-    let highscore = 0;
    if (!GuessNumber) {
         document.querySelector('.message').textContent = "⛔ No number!";
     }
@@ -20,16 +20,26 @@ Checkbutton.addEventListener('click', function () {
     document.querySelector('.highscore').textContent = highscore;
   } else if (GuessNumber > randomNumber) {
     document.querySelector('.message').textContent = "📈 Too high!";
+    score ++;
+    highscore ++;
+    document.querySelector('.score').textContent = score;
   } else if (GuessNumber < randomNumber) {
     document.querySelector('.message').textContent = "📉 Too low!";
+    score ++;
+    highscore ++;
+    document.querySelector('.score').textContent = score;
   }
 });
 
+
 AgainButton.addEventListener('click', function () {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+    score = 0;
     document.body.style.backgroundColor = '#222';
     document.querySelector('.number').textContent = '?';
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('.score').textContent = 0;
-    document.querySelector('.highscore').textContent = 0;
     inputNumber.value = '';
 });
+
+
